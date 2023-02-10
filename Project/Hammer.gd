d0c316eeee79d2e1e17029cb52503b16fccaebb8
daodalus <90871267+daodalus@users.noreply.github.com>
2023-02-10 12:11:50 -0500

    movemoney

--- Project/Hammer.gd
+++ Project/Hammer.gd
@@ -1,16 +1,13 @@
 extends KinematicBody2D
 
+export var power = 200
+var inAir = false
+onready var target = get_viewport().get_mouse_position()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func jump(mousePosition):
+	if(Input.is_action_just_pressed("left_click")):
+		target = mousePosition
+		inAir = true
+	if(inAir):position += (target - position)/10
+func _physics_process(delta):
+	jump(get_viewport().get_mouse_position())
